70731dfc5a89b1a5c07cab47a626226116c65f04
Mehrbod MK <mdgametronics@gmail.com>
2022-05-23 16:42:59 +0430

    Designed "Color" groupbox

--- MMK_CS_1_ColorGenerator/ColorGenerator.Designer.cs
+++ MMK_CS_1_ColorGenerator/ColorGenerator.Designer.cs
@@ -44,10 +44,23 @@
             this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.statusStrip_StatusBar = new System.Windows.Forms.StatusStrip();
-            this.groupBox_Color = new System.Windows.Forms.GroupBox();
             this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.groupBox_Color = new System.Windows.Forms.GroupBox();
+            this.trackBar_Color_Blue = new System.Windows.Forms.TrackBar();
+            this.label_Color_Blue = new System.Windows.Forms.Label();
+            this.trackBar_Color_Green = new System.Windows.Forms.TrackBar();
+            this.label_Color_Green = new System.Windows.Forms.Label();
+            this.trackBar_Color_Red = new System.Windows.Forms.TrackBar();
+            this.label_Color_Red = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.menuStrip_MainMenu.SuspendLayout();
             this.statusStrip_StatusBar.SuspendLayout();
+            this.groupBox_Color.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_Color_Blue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_Color_Green)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_Color_Red)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip_MainMenu
@@ -82,7 +95,7 @@
             this.newToolStripMenuItem.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.newToolStripMenuItem.Name = "newToolStripMenuItem";
             this.newToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
-            this.newToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
             this.newToolStripMenuItem.Text = "&New";
             // 
             // openToolStripMenuItem
@@ -91,13 +104,13 @@
             this.openToolStripMenuItem.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.openToolStripMenuItem.Name = "openToolStripMenuItem";
             this.openToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
-            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
             this.openToolStripMenuItem.Text = "&Open";
             // 
             // toolStripSeparator
             // 
             this.toolStripSeparator.Name = "toolStripSeparator";
-            this.toolStripSeparator.Size = new System.Drawing.Size(177, 6);
+            this.toolStripSeparator.Size = new System.Drawing.Size(143, 6);
             // 
             // saveToolStripMenuItem
             // 
@@ -105,24 +118,24 @@
             this.saveToolStripMenuItem.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
             this.saveToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
-            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
             this.saveToolStripMenuItem.Text = "&Save";
             // 
             // saveAsToolStripMenuItem
             // 
             this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
-            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
             this.saveAsToolStripMenuItem.Text = "Save &As";
             // 
             // toolStripSeparator1
             // 
             this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            this.toolStripSeparator1.Size = new System.Drawing.Size(143, 6);
             // 
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
             this.exitToolStripMenuItem.Text = "E&xit";
             // 
             // toolsToolStripMenuItem
@@ -137,13 +150,13 @@
             // customizeToolStripMenuItem
             // 
             this.customizeToolStripMenuItem.Name = "customizeToolStripMenuItem";
-            this.customizeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.customizeToolStripMenuItem.Size = new System.Drawing.Size(130, 22);
             this.customizeToolStripMenuItem.Text = "&Customize";
             // 
             // optionsToolStripMenuItem
             // 
             this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
-            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(130, 22);
             this.optionsToolStripMenuItem.Text = "&Options";
             // 
             // helpToolStripMenuItem
@@ -157,27 +170,18 @@
             // aboutToolStripMenuItem
             // 
             this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
-            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
             this.aboutToolStripMenuItem.Text = "&About...";
             // 
             // statusStrip_StatusBar
             // 
             this.statusStrip_StatusBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripStatusLabel1});
-            this.statusStrip_StatusBar.Location = new System.Drawing.Point(0, 312);
+            this.statusStrip_StatusBar.Location = new System.Drawing.Point(0, 348);
             this.statusStrip_StatusBar.Name = "statusStrip_StatusBar";
             this.statusStrip_StatusBar.Size = new System.Drawing.Size(527, 22);
             this.statusStrip_StatusBar.TabIndex = 1;
             this.statusStrip_StatusBar.Text = "statusStrip1";
-            // 
-            // groupBox_Color
-            // 
-            this.groupBox_Color.Location = new System.Drawing.Point(12, 27);
-            this.groupBox_Color.Name = "groupBox_Color";
-            this.groupBox_Color.Size = new System.Drawing.Size(252, 154);
-            this.groupBox_Color.TabIndex = 2;
-            this.groupBox_Color.TabStop = false;
-            this.groupBox_Color.Text = "Color";
             // 
             // toolStripStatusLabel1
             // 
@@ -185,21 +189,134 @@
             this.toolStripStatusLabel1.Size = new System.Drawing.Size(39, 17);
             this.toolStripStatusLabel1.Text = "Ready";
             // 
-            // Form1
+            // groupBox_Color
+            // 
+            this.groupBox_Color.Controls.Add(this.label3);
+            this.groupBox_Color.Controls.Add(this.label2);
+            this.groupBox_Color.Controls.Add(this.label1);
+            this.groupBox_Color.Controls.Add(this.trackBar_Color_Blue);
+            this.groupBox_Color.Controls.Add(this.label_Color_Blue);
+            this.groupBox_Color.Controls.Add(this.trackBar_Color_Green);
+            this.groupBox_Color.Controls.Add(this.label_Color_Green);
+            this.groupBox_Color.Controls.Add(this.trackBar_Color_Red);
+            this.groupBox_Color.Controls.Add(this.label_Color_Red);
+            this.groupBox_Color.Location = new System.Drawing.Point(12, 27);
+            this.groupBox_Color.Name = "groupBox_Color";
+            this.groupBox_Color.Size = new System.Drawing.Size(389, 187);
+            this.groupBox_Color.TabIndex = 2;
+            this.groupBox_Color.TabStop = false;
+            this.groupBox_Color.Text = "Color";
+            // 
+            // trackBar_Color_Blue
+            // 
+            this.trackBar_Color_Blue.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.trackBar_Color_Blue.Location = new System.Drawing.Point(53, 132);
+            this.trackBar_Color_Blue.Maximum = 255;
+            this.trackBar_Color_Blue.Name = "trackBar_Color_Blue";
+            this.trackBar_Color_Blue.Size = new System.Drawing.Size(302, 45);
+            this.trackBar_Color_Blue.TabIndex = 5;
+            this.trackBar_Color_Blue.TickStyle = System.Windows.Forms.TickStyle.TopLeft;
+            // 
+            // label_Color_Blue
+            // 
+            this.label_Color_Blue.AutoSize = true;
+            this.label_Color_Blue.ForeColor = System.Drawing.Color.Blue;
+            this.label_Color_Blue.Location = new System.Drawing.Point(6, 132);
+            this.label_Color_Blue.Name = "label_Color_Blue";
+            this.label_Color_Blue.Size = new System.Drawing.Size(33, 15);
+            this.label_Color_Blue.TabIndex = 4;
+            this.label_Color_Blue.Text = "Blue:";
+            // 
+            // trackBar_Color_Green
+            // 
+            this.trackBar_Color_Green.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.trackBar_Color_Green.Location = new System.Drawing.Point(53, 81);
+            this.trackBar_Color_Green.Maximum = 255;
+            this.trackBar_Color_Green.Name = "trackBar_Color_Green";
+            this.trackBar_Color_Green.Size = new System.Drawing.Size(302, 45);
+            this.trackBar_Color_Green.TabIndex = 3;
+            this.trackBar_Color_Green.TickStyle = System.Windows.Forms.TickStyle.TopLeft;
+            // 
+            // label_Color_Green
+            // 
+            this.label_Color_Green.AutoSize = true;
+            this.label_Color_Green.ForeColor = System.Drawing.Color.Green;
+            this.label_Color_Green.Location = new System.Drawing.Point(6, 81);
+            this.label_Color_Green.Name = "label_Color_Green";
+            this.label_Color_Green.Size = new System.Drawing.Size(41, 15);
+            this.label_Color_Green.TabIndex = 2;
+            this.label_Color_Green.Text = "Green:";
+            // 
+            // trackBar_Color_Red
+            // 
+            this.trackBar_Color_Red.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.trackBar_Color_Red.Location = new System.Drawing.Point(53, 30);
+            this.trackBar_Color_Red.Maximum = 255;
+            this.trackBar_Color_Red.Name = "trackBar_Color_Red";
+            this.trackBar_Color_Red.Size = new System.Drawing.Size(302, 45);
+            this.trackBar_Color_Red.TabIndex = 1;
+            this.trackBar_Color_Red.TickStyle = System.Windows.Forms.TickStyle.TopLeft;
+            // 
+            // label_Color_Red
+            // 
+            this.label_Color_Red.AutoSize = true;
+            this.label_Color_Red.ForeColor = System.Drawing.Color.Red;
+            this.label_Color_Red.Location = new System.Drawing.Point(6, 30);
+            this.label_Color_Red.Name = "label_Color_Red";
+            this.label_Color_Red.Size = new System.Drawing.Size(30, 15);
+            this.label_Color_Red.TabIndex = 0;
+            this.label_Color_Red.Text = "Red:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(361, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Green;
+            this.label2.Location = new System.Drawing.Point(361, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Blue;
+            this.label3.Location = new System.Drawing.Point(361, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "0";
+            // 
+            // ColorGenerator
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(527, 334);
+            this.ClientSize = new System.Drawing.Size(527, 370);
             this.Controls.Add(this.groupBox_Color);
             this.Controls.Add(this.statusStrip_StatusBar);
             this.Controls.Add(this.menuStrip_MainMenu);
             this.MainMenuStrip = this.menuStrip_MainMenu;
-            this.Name = "Form1";
+            this.Name = "ColorGenerator";
             this.Text = "Mehrbod M.K. C# Color Generator";
             this.menuStrip_MainMenu.ResumeLayout(false);
             this.menuStrip_MainMenu.PerformLayout();
             this.statusStrip_StatusBar.ResumeLayout(false);
             this.statusStrip_StatusBar.PerformLayout();
+            this.groupBox_Color.ResumeLayout(false);
+            this.groupBox_Color.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_Color_Blue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_Color_Green)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_Color_Red)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -224,5 +341,14 @@
         private StatusStrip statusStrip_StatusBar;
         private GroupBox groupBox_Color;
         private ToolStripStatusLabel toolStripStatusLabel1;
+        private Label label_Color_Red;
+        private TrackBar trackBar_Color_Red;
+        private TrackBar trackBar_Color_Blue;
+        private Label label_Color_Blue;
+        private TrackBar trackBar_Color_Green;
+        private Label label_Color_Green;
+        private Label label2;
+        private Label label1;
+        private Label label3;
     }
 }
